adf7026a42758778f8ef1d6c2b8faf317ae8cb7f
Mathias <mathias@localhost.localdomain>
2019-01-22 21:10:42 +0100

    refactors panel action component

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -4,12 +4,31 @@ import React from "react";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { Link } from "react-router-dom";
 
-export default class Action extends Component<{ icon: IconProp, to: string }> {
+export default class Action extends Component<{ icon: IconProp, to?: string, onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void }> {
+    constructor(props: { icon: IconProp, to?: string, onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void }) {
+        super(props)
+
+        this.onClick = this.onClick.bind(this)
+    }
+
+    private onClick(event: React.MouseEvent<HTMLButtonElement>) {
+        event.preventDefault()
+        if (this.props.onClick) this.props.onClick(event)
+    }
+
     public render() {
+        if (this.props.to) {
+            return (
+                <Link to={this.props.to} className="action-button btn btn-outline-dark">
+                    <FontAwesomeIcon icon={this.props.icon} />
+                </Link>
+            )
+        }
+
         return (
-            <Link to={this.props.to} className="action-button btn btn-outline-dark">
+            <button onClick={this.onClick} className="action-button btn btn-outline-dark">
                 <FontAwesomeIcon icon={this.props.icon} />
-            </Link>
+            </button>
         )
     }
 }
