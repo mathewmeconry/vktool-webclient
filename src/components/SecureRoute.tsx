f7f63296ae6e912e15adb201c54fcd152b0e9b70
Mathias <mathias@localhost.localdomain>
2019-02-11 12:28:12 +0100

    fixes #5 - detects if the user is authorized or not for a page - adds a 403 page

--- src/components/SecureRoute.tsx
+++ src/components/SecureRoute.tsx
@@ -5,9 +5,13 @@ import { State } from '../reducers/IndexReducer';
 import { AuthRoles } from '../interfaces/AuthRoles';
 import User from '../entities/User';
 import { RouteProps } from 'react-router';
+import { Dispatch } from 'redux';
+import { UI } from '../actions/UIActions';
+import { Error403 } from './Errors/403';
 
 export interface SecureRouteProps {
     user?: User,
+    showError?: (message: string) => void
     exact: boolean,
     path: string,
     roles: Array<AuthRoles>
@@ -24,6 +28,11 @@ export default class _SecureRoute extends Component<RouteProps & SecureRouteProp
             }
         }
 
+        if (this.props.user && this.props.showError) {
+            this.props.showError('Not Authorized!')
+            return <Route exact={this.props.exact} path={this.props.path} component={Error403} />
+        }
+
         return (
             <Route exact={this.props.exact} path={this.props.path} component={() => {
                 return (<Redirect push to={{
@@ -37,10 +46,18 @@ export default class _SecureRoute extends Component<RouteProps & SecureRouteProp
     }
 }
 
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        showError: (message: string) => {
+            dispatch(UI.showError(message))
+        }
+    }
+}
+
 const mapStateToProps = (state: State) => {
     return {
         user: state.data.user.data
     }
 }
 
-export const SecureRoute = connect(mapStateToProps)(_SecureRoute)
+export const SecureRoute = connect(mapStateToProps, mapDispatchToProps)(_SecureRoute)
