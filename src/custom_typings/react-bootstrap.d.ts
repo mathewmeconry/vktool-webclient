4e1b64a98018eee34395b70c89f8547abae5217d
Mathias <scherer.mat@gmail.com>
2019-02-11 17:21:16 +0100

    adds some typing hacks to get a successful build

---
+++ src/custom_typings/react-bootstrap.d.ts
@@ -0,0 +1,36 @@
+import React from "react";
+
+export type Omit<T, K> = {};
+
+declare module 'react-bootstrap' {
+    export type Omit<T, K> = {};
+
+    export type Sizes = 'xs' | 'xsmall' | 'sm' | 'small' | 'medium' | 'lg' | 'large';
+
+    export interface SelectCallback extends React.EventHandler<any> {
+        (eventKey: any, e: React.SyntheticEvent<{}>): void;
+        /**
+         * @deprecated
+         * This signature is a hack so can still derive from HTMLProps.
+         * It does not reflect the underlying event and should not be used.
+         */
+        (e: React.MouseEvent<{}>): void;
+    }
+
+    export interface TransitionCallbacks {
+        onEnter?(node: HTMLElement): any;
+        onEntered?(node: HTMLElement): any;
+        onEntering?(node: HTMLElement): any;
+        onExit?(node: HTMLElement): any;
+        onExited?(node: HTMLElement): any;
+        onExiting?(node: HTMLElement): any;
+    }
+
+    export namespace Button {
+        export interface ButtonProps {}
+    }
+
+    export namespace ButtonGroup {
+        export interface ButtonGroupProps {}
+    }
+}
