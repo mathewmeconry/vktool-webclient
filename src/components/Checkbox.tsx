5d866f20c54b17556c6b0bf78e88137e4df31953
Mathias Scherer <scherer.mat@gmail.com>
2019-06-12 13:11:19 +0200

    introduces new checkbox

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -4,8 +4,8 @@ import React from "react";
 export default class Checkbox extends Component<{ id: string, checked: boolean, label: string, onChange: (event: React.ChangeEvent<HTMLInputElement>) => void }> {
     public render() {
         return (
-            <span className="switch switch-sm">
-                <input id={this.props.id} type="checkbox" className="switch" checked={this.props.checked} onChange={this.props.onChange} />
+            <span>
+                <input id={this.props.id} type="checkbox" checked={this.props.checked} onChange={this.props.onChange} />
                 <label htmlFor="food">{this.props.label}</label>
             </span>
         )
