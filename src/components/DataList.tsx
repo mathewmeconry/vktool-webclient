743e09e3c3e75278b1320b6ab7828cf50156210b
Mathias Scherer <scherer.mat@gmail.com>
2020-01-28 01:55:39 +0100

    moves filter rendering to datalist

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,29 +1,33 @@
-import React, { Component } from "react";
-import { Page } from "../components/Page";
-import Row from "../components/Row";
-import Column from "../components/Column";
-import Panel from "../components/Panel";
-import { DataInterface } from "../reducers/DataReducer";
-import Table, { TableColumn } from "../components/Table";
-import { History } from "history";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Loading from "./Loading";
-import StringIndexed from "../interfaces/StringIndexed";
+import React, { Component } from "react"
+import { Page } from "../components/Page"
+import Row from "../components/Row"
+import Column from "../components/Column"
+import Panel from "../components/Panel"
+import { DataInterface } from "../reducers/DataReducer"
+import Table, { TableColumn, TableFilter } from "../components/Table"
+import { History } from "history"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Loading from "./Loading"
+import StringIndexed from "../interfaces/StringIndexed"
+import { ButtonToolbar, Button, ButtonGroup } from "react-bootstrap"
 
 export interface DataListProps<T> {
     data: DataInterface<T>,
     fetchData: Function,
-    onSearch?: Function,
+    onSearch?: (value: string) => void,
+    onFilter?: (filter: string) => void,
     title: string,
     viewLocation: string,
     tableColumns: Array<TableColumn>
     history: History
     panelActions?: Array<any>
     rowActions?: Array<any>,
+    filters?: TableFilter[]
 }
 
 export interface DataListState {
     searchString: string,
+    filter: string,
     sort: {
         keys: Array<string> | StringIndexed<string>,
         direction: 'asc' | 'desc'
@@ -41,13 +45,15 @@ export class DataList<T extends { id: string | number }> extends Component<DataL
         this.elementView = this.elementView.bind(this)
         this.textSearch = this.textSearch.bind(this)
         this.onSort = this.onSort.bind(this)
+        this.onFilter = this.onFilter.bind(this)
 
         this.state = {
             searchString: '',
             sort: {
                 keys: this.props.data.sort.keys,
                 direction: this.props.data.sort.direction
-            }
+            },
+            filter: (this.props.filters || [{ id: '' }])[0].id
         }
     }
 
@@ -67,6 +73,7 @@ export class DataList<T extends { id: string | number }> extends Component<DataL
     public shouldComponentUpdate(nextProps: DataListProps<T>, nextState: DataListState): boolean {
         if (this.props !== nextProps) return true
         if (this.state.searchString !== nextState.searchString) return true
+        if (this.state.filter !== nextState.filter) return true
 
         // don't rerender on sort change, because this is just to keep track of the state below
         return false
@@ -81,11 +88,33 @@ export class DataList<T extends { id: string | number }> extends Component<DataL
         })
     }
 
+    private onFilter(filter: string): void {
+        if (this.props.onFilter) this.props.onFilter(filter)
+        this.setState({
+            filter
+        })
+    }
+
     private textSearch(event: React.ChangeEvent<HTMLInputElement>) {
         if (this.props.onSearch) this.props.onSearch(event.target.value)
         this.setState({
             searchString: event.target.value
         })
+    }
+
+    private renderFilters() {
+        if (this.props.filters) {
+            return (
+                <ButtonGroup className="filters">
+                    {
+                        this.props.filters.map((filter: TableFilter) => {
+                            return <Button variant='outline-secondary' active={filter.id === this.state.filter} onClick={() => this.onFilter(filter.id)}>{filter.displayName}</Button>
+                        })
+                    }
+                </ButtonGroup>
+            )
+        }
+        return <ButtonGroup></ButtonGroup>
     }
 
     private renderTable() {
@@ -115,6 +144,8 @@ export class DataList<T extends { id: string | number }> extends Component<DataL
                 defaultSort={this.state.sort}
                 searchString={this.state.searchString}
                 onSort={this.onSort}
+                filters={this.props.filters}
+                defaultFilter={this.state.filter}
             />
         )
     }
@@ -127,7 +158,10 @@ export class DataList<T extends { id: string | number }> extends Component<DataL
                         <Panel
                             actions={this.props.panelActions || []}
                             title={
-                                <input id="search" value={this.state.searchString} placeholder="Search..." className="float-right form-control form-control-sm" onChange={(event) => this.textSearch(event)} />
+                                <ButtonToolbar className="justify-content-between align-items-center">
+                                    {this.renderFilters()}
+                                    <input id="search" value={this.state.searchString} placeholder="Search..." className="form-control form-control-sm" onChange={(event) => this.textSearch(event)} />
+                                </ButtonToolbar>
                             }>
                             {this.renderTable()}
                         </Panel>
