90cdd24d53da8362b918873c11683ea25ac3b762
Mathias Scherer <mathias.scherer@tamedia.ch>
2020-03-28 20:43:15 +0100

    adds redux-persist and fixes redux devtools integration

--- src/Root.tsx
+++ src/Root.tsx
@@ -9,6 +9,9 @@ import { SecureRoute } from "./components/SecureRoute"
 import Config from './Config'
 import { ConnectedRouter } from "connected-react-router"
 import { createBrowserHistory, History } from "history"
+import { Persistor } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import Loading from "./components/Loading"
 
 // styles
 import './styles/index.scss'
@@ -43,62 +46,67 @@ import { AddLogoff } from "./views/AddLogoff"
 
 export default class Root extends Component<{}, {}> {
     private store: Store<any, AnyAction>
+    private persistor: Persistor
     private history: History
 
     constructor(props: {}, state: {}) {
         super(props, state)
         Config.loadConfig()
         this.history = createBrowserHistory({ basename: '/webapp/' })
-        this.store = configureStore(this.history)
+        const { store, persistor } = configureStore(this.history)
+        this.store = store
+        this.persistor = persistor
         this.store.getState()
     }
 
     render() {
         return (
             <Provider store={this.store}>
-                <div className="body">
-                    <ToastContainer
-                        position="top-center"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={true}
-                        closeOnClick
-                        rtl={false}
-                        draggable={true}
-                        pauseOnHover={true}
-                    />
-                    <ConnectedRouter history={this.history} >
-                        <Switch>
-                            <Route exact path="/" component={() => { return (<Redirect to="/login" />) }} />
-                            <Route exact path="/login" component={Login} />
-                            <Route exact path="/login?source=:source" component={Login} />
-                            <SecureRoute exact path="/dashboard" roles={[AuthRoles.AUTHENTICATED]} component={Dashboard} />
-                            <SecureRoute exact path="/members" roles={[AuthRoles.MEMBERS_READ]} component={Members} />
-                            <SecureRoute exact path="/mailing-lists" roles={[AuthRoles.MAILING_LISTS]} component={MailingLists} />
-                            <SecureRoute exact path="/draft/collection-points" roles={[AuthRoles.DRAFT_READ]} component={CollectionPoints} />
-                            <SecureRoute exact path="/draft/collection-point/add" roles={[AuthRoles.DRAFT_EDIT, AuthRoles.DRAFT_CREATE]} component={AddCollectionPoint} />
-                            <SecureRoute exact path="/draft/logoffs" roles={[AuthRoles.LOGOFFS_READ]} component={Logoffs} />
-                            <SecureRoute exact path="/draft/logoff/add" roles={[AuthRoles.LOGOFFS_CREATE]} component={AddLogoff} />
-                            <SecureRoute exact path="/draft/logoff/:id" roles={[AuthRoles.LOGOFFS_READ, AuthRoles.AUTHENTICATED]} component={Logoff} />
-                            <SecureRoute exact path="/contact/:id" roles={[AuthRoles.CONTACTS_READ, AuthRoles.MEMBERS_READ, AuthRoles.AUTHENTICATED]} component={Contact} />
-                            <SecureRoute exact path="/orders" roles={[AuthRoles.ORDERS_READ]} component={Orders} />
-                            <SecureRoute exact path="/order/:id" roles={[AuthRoles.ORDERS_READ]} component={Order} />
-                            <SecureRoute exact path="/billing-reports" roles={[AuthRoles.BILLINGREPORTS_READ, AuthRoles.BILLINGREPORTS_CREATE]} component={BillingReports} />
-                            <SecureRoute exact path="/billing-reports/add" roles={[AuthRoles.BILLINGREPORTS_CREATE]} component={AddBillingReport} />
-                            <SecureRoute exact path="/billing-report/:id" roles={[AuthRoles.BILLINGREPORTS_CREATE, AuthRoles.BILLINGREPORTS_READ]} component={BillingReport} />
-                            <SecureRoute exact path="/compensations" roles={[AuthRoles.COMPENSATIONS_READ]} component={Compensations} />
-                            <SecureRoute exact path="/compensations/add" roles={[AuthRoles.COMPENSATIONS_CREATE]} component={AddCompensation} />
-                            <SecureRoute exact path="/compensation/:id" roles={[AuthRoles.COMPENSATIONS_READ, AuthRoles.AUTHENTICATED]} component={Compensation} />
-                            <SecureRoute exact path="/payouts" roles={[AuthRoles.PAYOUTS_READ]} component={Payouts} />
-                            <SecureRoute exact path="/payouts/add" roles={[AuthRoles.PAYOUTS_CREATE]} component={AddPayout} />
-                            <SecureRoute exact path="/payout/:id" roles={[AuthRoles.PAYOUTS_READ]} component={Payout} />
-                            <SecureRoute exact path="/payout/:id/:memberId" roles={[AuthRoles.PAYOUTS_READ]} component={PayoutMember} />
-                            <SecureRoute exact path="/users" roles={[AuthRoles.ADMIN]} component={Users} />
-                            <SecureRoute exact path="/user/:id" roles={[AuthRoles.ADMIN]} component={User} />
-                            <Route path="/*" component={Error404} />
-                        </Switch>
-                    </ConnectedRouter>
-                </div>
+                <PersistGate persistor={this.persistor} loading={<Loading />}>
+                    <div className="body">
+                        <ToastContainer
+                            position="top-center"
+                            autoClose={5000}
+                            hideProgressBar={false}
+                            newestOnTop={true}
+                            closeOnClick
+                            rtl={false}
+                            draggable={true}
+                            pauseOnHover={true}
+                        />
+                        <ConnectedRouter history={this.history} >
+                            <Switch>
+                                <Route exact path="/" component={() => { return (<Redirect to="/login" />) }} />
+                                <Route exact path="/login" component={Login} />
+                                <Route exact path="/login?source=:source" component={Login} />
+                                <SecureRoute exact path="/dashboard" roles={[AuthRoles.AUTHENTICATED]} component={Dashboard} />
+                                <SecureRoute exact path="/members" roles={[AuthRoles.MEMBERS_READ]} component={Members} />
+                                <SecureRoute exact path="/mailing-lists" roles={[AuthRoles.MAILING_LISTS]} component={MailingLists} />
+                                <SecureRoute exact path="/draft/collection-points" roles={[AuthRoles.DRAFT_READ]} component={CollectionPoints} />
+                                <SecureRoute exact path="/draft/collection-point/add" roles={[AuthRoles.DRAFT_EDIT, AuthRoles.DRAFT_CREATE]} component={AddCollectionPoint} />
+                                <SecureRoute exact path="/draft/logoffs" roles={[AuthRoles.LOGOFFS_READ]} component={Logoffs} />
+                                <SecureRoute exact path="/draft/logoff/add" roles={[AuthRoles.LOGOFFS_CREATE]} component={AddLogoff} />
+                                <SecureRoute exact path="/draft/logoff/:id" roles={[AuthRoles.LOGOFFS_READ, AuthRoles.AUTHENTICATED]} component={Logoff} />
+                                <SecureRoute exact path="/contact/:id" roles={[AuthRoles.CONTACTS_READ, AuthRoles.MEMBERS_READ, AuthRoles.AUTHENTICATED]} component={Contact} />
+                                <SecureRoute exact path="/orders" roles={[AuthRoles.ORDERS_READ]} component={Orders} />
+                                <SecureRoute exact path="/order/:id" roles={[AuthRoles.ORDERS_READ]} component={Order} />
+                                <SecureRoute exact path="/billing-reports" roles={[AuthRoles.BILLINGREPORTS_READ, AuthRoles.BILLINGREPORTS_CREATE]} component={BillingReports} />
+                                <SecureRoute exact path="/billing-reports/add" roles={[AuthRoles.BILLINGREPORTS_CREATE]} component={AddBillingReport} />
+                                <SecureRoute exact path="/billing-report/:id" roles={[AuthRoles.BILLINGREPORTS_CREATE, AuthRoles.BILLINGREPORTS_READ]} component={BillingReport} />
+                                <SecureRoute exact path="/compensations" roles={[AuthRoles.COMPENSATIONS_READ]} component={Compensations} />
+                                <SecureRoute exact path="/compensations/add" roles={[AuthRoles.COMPENSATIONS_CREATE]} component={AddCompensation} />
+                                <SecureRoute exact path="/compensation/:id" roles={[AuthRoles.COMPENSATIONS_READ, AuthRoles.AUTHENTICATED]} component={Compensation} />
+                                <SecureRoute exact path="/payouts" roles={[AuthRoles.PAYOUTS_READ]} component={Payouts} />
+                                <SecureRoute exact path="/payouts/add" roles={[AuthRoles.PAYOUTS_CREATE]} component={AddPayout} />
+                                <SecureRoute exact path="/payout/:id" roles={[AuthRoles.PAYOUTS_READ]} component={Payout} />
+                                <SecureRoute exact path="/payout/:id/:memberId" roles={[AuthRoles.PAYOUTS_READ]} component={PayoutMember} />
+                                <SecureRoute exact path="/users" roles={[AuthRoles.ADMIN]} component={Users} />
+                                <SecureRoute exact path="/user/:id" roles={[AuthRoles.ADMIN]} component={User} />
+                                <Route path="/*" component={Error404} />
+                            </Switch>
+                        </ConnectedRouter>
+                    </div>
+                </PersistGate>
             </Provider>
         )
     }
--- src/Store.ts
+++ src/Store.ts
@@ -1,15 +1,26 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import IndexReducer from "./reducers/IndexReducer";
+import { createStore, applyMiddleware, combineReducers } from "redux"
+import IndexReducer from "./reducers/IndexReducer"
 import thunkMiddleware from 'redux-thunk'
 import { connectRouter, routerMiddleware } from 'connected-react-router'
-import { History } from "history";
-import { composeWithDevTools } from "remote-redux-devtools";
+import { History } from "history"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { persistStore, persistReducer } from 'redux-persist'
+import localStorage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+    key: 'root',
+    storage: localStorage,
+    blacklist: ['router']
+}
 
 export default function configureStore(history: History<any>) {
-    return createStore(
-        combineReducers({ ...IndexReducer, router: connectRouter(history) }),
+    const persistedReducer = persistReducer(persistConfig, combineReducers({ ...IndexReducer, router: connectRouter(history) }))
+
+    const store = createStore(
+        persistedReducer,
         composeWithDevTools(
             applyMiddleware(thunkMiddleware, routerMiddleware(history))
         )
     )
+    return { store, persistor: persistStore(store) }
 }
