c708ae1c3d02be9dd5ddf9c72738579e7844a159
Mathias Scherer <mathias.scherer@tamedia.ch>
2020-05-29 19:22:53 +0200

    switches to apollo batch http link for more speed

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,16 @@
-import React, { Component } from "react"
+import React from "react"
 import { Provider } from "react-redux"
 import configureStore from "./Store"
-import { Store, AnyAction } from "redux"
 import { Route, Switch, Redirect } from "react-router-dom"
 import { ToastContainer } from 'react-toastify'
 import { AuthRoles } from "./interfaces/AuthRoles"
 import SecureRoute from "./components/SecureRoute"
 import Config from './Config'
 import { ConnectedRouter } from "connected-react-router"
-import { createBrowserHistory, History } from "history"
-import ApolloClient from 'apollo-boost'
+import { createBrowserHistory } from "history"
+import { ApolloLink, ApolloClient, InMemoryCache } from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { BatchHttpLink } from 'apollo-link-batch-http'
 
 // styles
 import './styles/index.scss'
@@ -42,74 +42,84 @@ import AddPayout from "./views/AddPayout"
 import Logoffs from "./views/Logoffs"
 import Logoff from "./views/Logoff"
 import AddLogoff from "./views/AddLogoff"
+import { onError } from "apollo-link-error"
+import { UI } from "./actions/UIActions"
 
-export default class Root extends Component<{}, {}> {
-    private store: Store<any, AnyAction>
-    private history: History
-    private apolloClient: ApolloClient<unknown>
+export default function Root() {
+    Config.loadConfig()
+    const history = createBrowserHistory({ basename: '/webapp/' })
+    const { store } = configureStore(history)
+    store.getState()
 
-    constructor(props: {}, state: {}) {
-        super(props, state)
-        Config.loadConfig()
-        this.history = createBrowserHistory({ basename: '/webapp/' })
-        const { store } = configureStore(this.history)
-        this.store = store
-        this.store.getState()
-        this.apolloClient = new ApolloClient({
-            uri: `${Config.apiEndpoint}/graphql`,
-            credentials: 'include',
-        })
-    }
+    const httpLink = new BatchHttpLink({ credentials: 'include', uri: `${Config.apiEndpoint}/graphql` })
+    const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+        if (graphQLErrors)
+            graphQLErrors.map(({ message, path }) => {
+                if (path && path[0] === 'me' && message.indexOf('Access denied!') > -1 && history.location.pathname !== '/login') {
+                    history.push('/login', { unAuthenticated: true, prevLocation: history.location.pathname })
+                    return
+                }
 
-    render() {
-        return (
-            <Provider store={this.store}>
-                <div className="body">
-                    <ToastContainer
-                        position="top-center"
-                        autoClose={5000}
-                        hideProgressBar={false}
-                        newestOnTop={true}
-                        closeOnClick
-                        rtl={false}
-                        draggable={true}
-                        pauseOnHover={true}
-                    />
-                    <ConnectedRouter history={this.history} >
-                        <ApolloProvider client={this.apolloClient}>
-                            <Switch>
-                                <Route exact path="/" component={() => { return (<Redirect to="/login" />) }} />
-                                <Route exact path="/login" component={Login} />
-                                <Route exact path="/login?source=:source" component={Login} />
-                                <SecureRoute exact path="/dashboard" roles={[AuthRoles.AUTHENTICATED]} component={Dashboard} />
-                                <SecureRoute exact path="/members" roles={[AuthRoles.MEMBERS_READ]} component={Members} />
-                                <SecureRoute exact path="/mailing-lists" roles={[AuthRoles.MAILING_LISTS]} component={MailingLists} />
-                                <SecureRoute exact path="/draft/collection-points" roles={[AuthRoles.DRAFT_READ]} component={CollectionPoints} />
-                                <SecureRoute exact path="/draft/collection-point/add" roles={[AuthRoles.DRAFT_EDIT, AuthRoles.DRAFT_CREATE]} component={AddCollectionPoint} />
-                                <SecureRoute exact path="/draft/logoffs" roles={[AuthRoles.LOGOFFS_READ]} component={Logoffs} />
-                                <SecureRoute exact path="/draft/logoff/add" roles={[AuthRoles.LOGOFFS_CREATE]} component={AddLogoff} />
-                                <SecureRoute exact path="/draft/logoff/:id" roles={[AuthRoles.LOGOFFS_READ, AuthRoles.AUTHENTICATED]} component={Logoff} />
-                                <SecureRoute exact path="/contact/:id" roles={[AuthRoles.CONTACTS_READ, AuthRoles.MEMBERS_READ, AuthRoles.AUTHENTICATED]} component={Contact} />
-                                <SecureRoute exact path="/orders" roles={[AuthRoles.ORDERS_READ]} component={Orders} />
-                                <SecureRoute exact path="/order/:id" roles={[AuthRoles.ORDERS_READ]} component={Order} />
-                                <SecureRoute exact path="/billing-reports" roles={[AuthRoles.BILLINGREPORTS_READ, AuthRoles.BILLINGREPORTS_CREATE]} component={BillingReports} />
-                                <SecureRoute exact path="/billing-reports/add" roles={[AuthRoles.BILLINGREPORTS_CREATE]} component={AddBillingReport} />
-                                <SecureRoute exact path="/billing-report/:id" roles={[AuthRoles.BILLINGREPORTS_CREATE, AuthRoles.BILLINGREPORTS_READ]} component={BillingReport} />
-                                <SecureRoute exact path="/compensations" roles={[AuthRoles.COMPENSATIONS_READ]} component={Compensations} />
-                                <SecureRoute exact path="/compensations/add" roles={[AuthRoles.COMPENSATIONS_CREATE]} component={AddCompensation} />
-                                <SecureRoute exact path="/compensation/:id" roles={[AuthRoles.COMPENSATIONS_READ, AuthRoles.AUTHENTICATED]} component={Compensation} />
-                                <SecureRoute exact path="/payouts" roles={[AuthRoles.PAYOUTS_READ]} component={Payouts} />
-                                <SecureRoute exact path="/payouts/add" roles={[AuthRoles.PAYOUTS_CREATE]} component={AddPayout} />
-                                <SecureRoute exact path="/payout/:id" roles={[AuthRoles.PAYOUTS_READ]} component={Payout} />
-                                <SecureRoute exact path="/payout/:id/:memberId" roles={[AuthRoles.PAYOUTS_READ]} component={PayoutMember} />
-                                <SecureRoute exact path="/users" roles={[AuthRoles.ADMIN]} component={Users} />
-                                <SecureRoute exact path="/user/:id" roles={[AuthRoles.ADMIN]} component={User} />
-                                <Route path="/*" component={Error404} />
-                            </Switch>
-                        </ApolloProvider>
-                    </ConnectedRouter>
-                </div>
-            </Provider>
-        )
-    }
+                console.error(`[GraphQL error]: Message: ${message}, Operation: ${operation.operationName}, Path: ${path}`)
+            })
+        if (networkError) {
+            store.dispatch(UI.showError('Etwas ist schief gelaufen'))
+            console.error(`[Network error]: ${networkError}`)
+        }
+    })
+
+    const apolloClient = new ApolloClient({
+        link: ApolloLink.from([errorLink, httpLink]),
+        cache: new InMemoryCache()
+    })
+
+    return (
+        <Provider store={store}>
+            <div className="body">
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={true}
+                    closeOnClick
+                    rtl={false}
+                    draggable={true}
+                    pauseOnHover={true}
+                />
+                <ConnectedRouter history={history} >
+                    <ApolloProvider client={apolloClient}>
+                        <Switch>
+                            <Route exact path="/" component={() => { return (<Redirect to="/login" />) }} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/login?source=:source" component={Login} />
+                            <SecureRoute exact path="/dashboard" roles={[AuthRoles.AUTHENTICATED]} component={Dashboard} />
+                            <SecureRoute exact path="/members" roles={[AuthRoles.MEMBERS_READ]} component={Members} />
+                            <SecureRoute exact path="/mailing-lists" roles={[AuthRoles.MAILING_LISTS]} component={MailingLists} />
+                            <SecureRoute exact path="/draft/collection-points" roles={[AuthRoles.DRAFT_READ]} component={CollectionPoints} />
+                            <SecureRoute exact path="/draft/collection-point/add" roles={[AuthRoles.DRAFT_EDIT, AuthRoles.DRAFT_CREATE]} component={AddCollectionPoint} />
+                            <SecureRoute exact path="/draft/logoffs" roles={[AuthRoles.LOGOFFS_READ]} component={Logoffs} />
+                            <SecureRoute exact path="/draft/logoff/add" roles={[AuthRoles.LOGOFFS_CREATE]} component={AddLogoff} />
+                            <SecureRoute exact path="/draft/logoff/:id" roles={[AuthRoles.LOGOFFS_READ, AuthRoles.AUTHENTICATED]} component={Logoff} />
+                            <SecureRoute exact path="/contact/:id" roles={[AuthRoles.CONTACTS_READ, AuthRoles.MEMBERS_READ, AuthRoles.AUTHENTICATED]} component={Contact} />
+                            <SecureRoute exact path="/orders" roles={[AuthRoles.ORDERS_READ]} component={Orders} />
+                            <SecureRoute exact path="/order/:id" roles={[AuthRoles.ORDERS_READ]} component={Order} />
+                            <SecureRoute exact path="/billing-reports" roles={[AuthRoles.BILLINGREPORTS_READ, AuthRoles.BILLINGREPORTS_CREATE]} component={BillingReports} />
+                            <SecureRoute exact path="/billing-reports/add" roles={[AuthRoles.BILLINGREPORTS_CREATE]} component={AddBillingReport} />
+                            <SecureRoute exact path="/billing-report/:id" roles={[AuthRoles.BILLINGREPORTS_CREATE, AuthRoles.BILLINGREPORTS_READ]} component={BillingReport} />
+                            <SecureRoute exact path="/compensations" roles={[AuthRoles.COMPENSATIONS_READ]} component={Compensations} />
+                            <SecureRoute exact path="/compensations/add" roles={[AuthRoles.COMPENSATIONS_CREATE]} component={AddCompensation} />
+                            <SecureRoute exact path="/compensation/:id" roles={[AuthRoles.COMPENSATIONS_READ, AuthRoles.AUTHENTICATED]} component={Compensation} />
+                            <SecureRoute exact path="/payouts" roles={[AuthRoles.PAYOUTS_READ]} component={Payouts} />
+                            <SecureRoute exact path="/payouts/add" roles={[AuthRoles.PAYOUTS_CREATE]} component={AddPayout} />
+                            <SecureRoute exact path="/payout/:id" roles={[AuthRoles.PAYOUTS_READ]} component={Payout} />
+                            <SecureRoute exact path="/payout/:id/:memberId" roles={[AuthRoles.PAYOUTS_READ]} component={PayoutMember} />
+                            <SecureRoute exact path="/users" roles={[AuthRoles.ADMIN]} component={Users} />
+                            <SecureRoute exact path="/user/:id" roles={[AuthRoles.ADMIN]} component={User} />
+                            <Route path="/*" component={Error404} />
+                        </Switch>
+                    </ApolloProvider>
+                </ConnectedRouter>
+            </div>
+        </Provider>
+    )
 }
